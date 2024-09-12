efb0c4c4b2d97b61ddafef58d82e0e1c951c407d
Gabriel <106557375+HollowGbrl@users.noreply.github.com>
2024-09-12 11:15:53 -0300

    Update ajustador_glorys.py

--- python codes/ajustador_glorys.py
+++ python codes/ajustador_glorys.py
@@ -1,8 +1,24 @@
+#codigo feito para gerar arquivos de timeseries a partir de dados de netcdf (no caso, dados de reanalise do GLORYS) para inserir ao modelo como pontos na borda.
+#pode ser usado para extrair os pontos de um arquivo  .pli de borda do Delft3D
 #o codigo atualmente suporta apenas um arquivo de boundary, com uma boundary por vez, e apenas um arquivo netcdf de dados
 import xarray as xr 
 import time
 import pandas as pd
 from pyproj import Transformer
+
+
+#arquivo netcdf com todos os pontos, o arquivo deve conter as variaveis zos, uo e vos, e as dimensões latitude, longitude e time
+file = 'dados_glorys/seasurfacegeoid_u_v/cmems_mod_glo_phy_my_0.083deg_P1D-m_1718996170742.nc'
+#pasta para onde será salvo os arquivos processados
+pasta = 'dados_glorys/processamento'
+#pasta para onde será salvo os arquivos processados com nivel ajustado
+save = 'dados_glorys/process0/'
+#arquivo pli (boundary) com os pontos de interesse
+arquivopli = '../finaltcc/delft/51_astroonly_1borda.dsproj_data/FlowFM/input/Boundary01.pli'
+#projeção geografica dos pontos do arquivo pli
+proj = 'EPSG:32722'
+
+
 
 #função para ler o arquivo pli e extrair os pontos
 def geralocais(arquivopli, projcord):
@@ -20,7 +36,7 @@ def geralocais(arquivopli, projcord):
                 nlat, nlon = transformer.transform(lon, lat)
                 locais.append((nlat, nlon, name))
     return  locais
-#fução para ler o arquivo netcdf e extrair os dados
+#Função para ler o arquivo netcdf e extrair os dados
 def netcdfprocess(file, locais, pasta):
     #criando varios arquivos time series para cada ponto da região de interesse
     for regiao in locais:
@@ -49,23 +65,15 @@ def adjustzero(pasta, save):
         df.to_csv(f'{save}{file}', index=False)
     return print('ajustado o nivel zero')
 
-#arquivo netcdf com todos os pontos, o arquivo deve conter as variaveis zos, uo e vos, e as dimensões latitude, longitude e time
-file = 'dados_glorys/seasurfacegeoid_u_v/cmems_mod_glo_phy_my_0.083deg_P1D-m_1718996170742.nc'
-#pasta para onde será salvo os arquivos processados
-pasta = 'dados_glorys/processamento'
-#pasta para onde será salvo os arquivos processados com nivel ajustado
-save = 'dados_glorys/process0/'
-#arquivo pli (boundary) com os pontos de interesse
-arquivopli = '../finaltcc/delft/51_astroonly_1borda.dsproj_data/FlowFM/input/Boundary01.pli'
-#projeção geografica dos pontos do arquivo pli
-proj = 'EPSG:32722'
 
 
 #chamando as funções
 locais = geralocais(arquivopli, proj)
 #netcdfprocess(file, locais, pasta)
+#adjustzero(pasta, save)
+#gerando apenas um txt simples com os pontos usados
 with open ('dados_glorys/locais.txt', 'w') as saida:
     saida.write('lat lon name\n')
     for local in locais:
         saida.write(f'{round(local[0], 2)} {round(local[1],2)} {local[2]}\n')
-#adjustzero(pasta, save)
+
